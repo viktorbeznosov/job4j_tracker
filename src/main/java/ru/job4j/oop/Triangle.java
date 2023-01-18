2cddced11d972a069729330cc7ef30a6e4926b71
viktorbeznosov <viktorbeznosov@mail.ru>
2023-01-18 22:32:49 +0300

    2.1. Рефакторинг - Площадь треугольника. [#122642 #382296]

--- src/main/java/ru/job4j/oop/Triangle.java
+++ src/main/java/ru/job4j/oop/Triangle.java
@@ -21,7 +21,6 @@ public class Triangle {
     }
 
     public double area() {
-        double rsl = -1;
         double ab = first.distance(second);
         double ac = first.distance(third);
         double bc = second.distance(third);
@@ -29,6 +28,6 @@ public class Triangle {
             double p = semiPerimeter(ab, ac, bc);
             return Math.sqrt(p * (p - ab) * (p - ac) * (p - bc));
         }
-        return rsl;
+        return -1;
     }
 }

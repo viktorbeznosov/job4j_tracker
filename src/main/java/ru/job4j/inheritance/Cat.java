8ddea7586b22a861ba6f76e529d0eb0a488a79b2
viktorbeznosov <viktorbeznosov@mail.ru>
2023-01-21 17:14:33 +0300

    6. Вызов переопределенного метода родителя - super.method ... [#504871]

--- src/main/java/ru/job4j/inheritance/Cat.java
+++ src/main/java/ru/job4j/inheritance/Cat.java
@@ -1,9 +1,17 @@
 package ru.job4j.inheritance;
 
-public class Cat {
-
+public class Cat extends Animal {
     public boolean canPurr() {
         return true;
+    }
+
+    @Override
+    public void instanceInvoke() {
+        System.out.println("Вызов метода экземпляра Cat");
+    }
+
+    public static void staticInvoke() {
+        System.out.println("Вызов статического метода Cat");
     }
 
 }
---
+++ src/main/java/ru/job4j/inheritance/HideExampleMain.java
@@ -0,0 +1,18 @@
+package ru.job4j.inheritance;
+
+public class HideExampleMain {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        Animal cat = new Cat();
+        animal.instanceInvoke();
+        cat.instanceInvoke();
+        Animal.staticInvoke();
+        Cat.staticInvoke();
+
+        cat.staticInvoke();
+        Animal other = null;
+        other.staticInvoke();
+        Cat kitty = new Cat();
+        kitty.staticInvoke();
+    }
+}

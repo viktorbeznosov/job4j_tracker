775e1843eb5f7f0afbdc5d734d93650efbd81ddc
viktorbeznosov <viktorbeznosov@mail.ru>
2023-01-22 16:17:07 +0300

    8. Date. Отображение даты. [#310017 #383539]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 public class Item {
     private int id;
     private String name;
-    private LocalDateTime created;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
     }
@@ -17,7 +17,6 @@ public class Item {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
-        this.created = LocalDateTime.now();
     }
 
     public int getId() {

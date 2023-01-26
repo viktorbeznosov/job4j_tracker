2b19fca078fe28c1e4f27c3e286f86765710c1d2
viktorbeznosov <viktorbeznosov@mail.ru>
2023-01-26 21:13:09 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #384734]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -57,8 +57,9 @@ public class Tracker {
     }
 
     public boolean delete(int id) {
-        if (indexOf(id) != -1) {
-            System.arraycopy(items, indexOf(id) + 1, items, indexOf(id), items.length - (indexOf(id) + 1));
+        int index = indexOf(id);
+        if (index != -1) {
+            System.arraycopy(items, index + 1, items, index, items.length - (index + 1));
             items[items.length - 1] = null;
             size--;
             return true;

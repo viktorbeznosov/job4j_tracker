bf9ff33c43f258ee2ce1cf04d9e434d18595608a
viktorbeznosov <viktorbeznosov@mail.ru>
2023-01-26 22:30:48 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,6 +7,7 @@ public class StartUI {
     public static void main(String[] args) {
         Item item = new Item(1, "Item with date");
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        System.out.println(item);
         System.out.println(item.getCreated().format(formatter));
     }
 }

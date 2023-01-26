66421e85fe776a5ac14f3c5c9de1277be1b1ee71
viktorbeznosov <viktorbeznosov@mail.ru>
2023-01-26 23:09:18 +0300

    11. Record [#504880 #385422]

---
+++ src/main/java/ru/job4j/record/PersonRecord.java
@@ -0,0 +1,24 @@
+package ru.job4j.record;
+
+public record PersonRecord(String name, int age) implements Comparable<PersonRecord> {
+    public static int maxAge = 100;
+
+    public PersonRecord {
+        if (age > 101) {
+            throw new IllegalArgumentException("Возраст должен быть менее 101");
+        }
+    }
+
+    public static int getMaxAge() {
+        return maxAge;
+    }
+
+    public void info() {
+        System.out.println("Напечатать информацию");
+    }
+
+    @Override
+    public int compareTo(PersonRecord o) {
+        return name.compareTo(o.name);
+    }
+}

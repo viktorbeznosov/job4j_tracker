90318831911bcc00167a52aca7abec6ef895372a
viktorbeznosov <viktorbeznosov@mail.ru>
2023-02-01 00:08:12 +0300

    4.2. Интерфейс Input [#181092 #386639]

--- src/main/java/ru/job4j/oop/HierarchyUsage.java
+++ src/main/java/ru/job4j/oop/HierarchyUsage.java
@@ -2,6 +2,7 @@ package ru.job4j.oop;
 
 public class HierarchyUsage {
     public static void main(String[] args) {
+        
         Car car = new Car();
         Transport tp = car;
         System.out.println(tp.getClass());

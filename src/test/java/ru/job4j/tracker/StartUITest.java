3e8986d81d1852849711e6f710daa80955d4870b
viktorbeznosov <viktorbeznosov@mail.ru>
2023-02-03 22:33:22 +0300

    12.1 Зависимость от System.out [#33568 #388045]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -4,49 +4,5 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class StartUITest {
-//    @Test
-//    public void whenCreateItem() {
-//        Input in = new StubInput(
-//                new String[] {"0", "Item name", "1"}
-//        );
-//        Tracker tracker = new Tracker();
-//        UserAction[] actions = {
-//                new CreateAction(out),
-//                new ExitProgram()
-//        };
-//        new StartUI().init(in, tracker, actions);
-//        assertThat(tracker.findAll()[0].getName()).isEqualTo("Item name");
-//    }
-
-//    @Test
-//    public void whenReplaceItem() {
-//        Tracker tracker = new Tracker();
-//        Item item = tracker.add(new Item("Replaced item"));
-//        String replacedName = "New item name";
-//        Input in = new StubInput(
-//                new String[] {"0", String.valueOf(item.getId()), "New item name", "1"}
-//        );
-//        UserAction[] actions = {
-//                new EditItem(out),
-//                new ExitProgram()
-//        };
-//        new StartUI().init(in, tracker, actions);
-//        assertThat(tracker.findById(item.getId()).getName()).isEqualTo(replacedName);
-//    }
-
-//    @Test
-//    public void whenDeleteItem() {
-//        Tracker tracker = new Tracker();
-//        Item item = tracker.add(new Item("Deleted item"));
-//        Input in = new StubInput(
-//                new String[] {"0", String.valueOf(item.getId()), "1"}
-//        );
-//        UserAction[] actions = {
-//                new DeleteItem(out),
-//                new ExitProgram()
-//        };
-//        new StartUI().init(in, tracker, actions);
-//        assertThat(tracker.findById(item.getId())).isNull();
-//    }
 
 }

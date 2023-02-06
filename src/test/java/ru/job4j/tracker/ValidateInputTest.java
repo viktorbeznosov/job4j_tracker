e8d189b0eeeefbc3d11fe3b01d33c39b56a1a9cd
viktorbeznosov <viktorbeznosov@mail.ru>
2023-02-06 20:20:24 +0300

    2.1. Тест на ValidateInput [#311887 #388590]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -46,7 +46,9 @@ class ValidateInputTest {
                 new String[] {"1", "2"}
         );
         ValidateInput input = new ValidateInput(out, in);
-        int selected = input.askInt("Enter menu:");
-        assertThat(selected).isEqualTo(1);
+        int selected1 = input.askInt("Enter menu:");
+        int selected2 = input.askInt("Enter menu:");
+        assertThat(selected1).isEqualTo(1);
+        assertThat(selected2).isEqualTo(2);
     }
 }

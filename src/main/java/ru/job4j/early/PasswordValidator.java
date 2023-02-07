bec00f42e1425928845e445461120bfdaa2c3e86
viktorbeznosov <viktorbeznosov@mail.ru>
2023-02-07 22:39:41 +0300

    4. Принципы раннего возврата и охранных выражений [#504875 #388630]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -1,6 +1,14 @@
 package ru.job4j.early;
 
 public class PasswordValidator {
+    private enum PasswordError {
+        PASSWORD_NOT_CONTAINS_UPPERCASE_LETTER,
+        PASSWORD_NOT_CONTAINS_LOWERCASE_LETTER,
+        PASSWORD_NOT_CONTAINS_DIGIT,
+        PASSWORD_NOT_CONTAINS_SPECIAL_SYMBOL,
+        PASSWORD_IS_CORRECT
+    }
+
     public static String validate(String password) {
         if (password == null) {
             throw new IllegalArgumentException("Password can't be null");
@@ -10,19 +18,19 @@ public class PasswordValidator {
             throw new IllegalArgumentException("Password should be length [8, 32]");
         }
 
-        if (!containsUppercaseLetter(password)) {
+        if (validatePasswordLetters(password).equals(PasswordError.PASSWORD_NOT_CONTAINS_UPPERCASE_LETTER)) {
             throw new IllegalArgumentException("Password should contain at least one uppercase letter");
         }
 
-        if (!containsLowercaseLetter(password)) {
+        if (validatePasswordLetters(password).equals(PasswordError.PASSWORD_NOT_CONTAINS_LOWERCASE_LETTER)) {
             throw new IllegalArgumentException("Password should contain at least one lowercase letter");
         }
 
-        if (!containsDigit(password)) {
+        if (validatePasswordLetters(password).equals(PasswordError.PASSWORD_NOT_CONTAINS_DIGIT)) {
             throw new IllegalArgumentException("Password should contain at least one figure");
         }
 
-        if (!containsSpecialSymbol(password)) {
+        if (validatePasswordLetters(password).equals(PasswordError.PASSWORD_NOT_CONTAINS_SPECIAL_SYMBOL)) {
             throw new IllegalArgumentException("Password should contain at least one special symbol");
         }
 
@@ -33,45 +41,40 @@ public class PasswordValidator {
         return password;
     }
 
-    public static boolean containsUppercaseLetter(String password) {
+    public static PasswordError validatePasswordLetters(String password) {
+        boolean containsUppercaseLetter = false;
+        boolean containsLowercaseLetter = false;
+        boolean containsDigit = false;
+        boolean containsSpecialSymbol = false;
         for (int i = 0; i < password.length(); i++) {
             if (Character.isUpperCase(password.charAt(i))) {
-                return true;
+                containsUppercaseLetter = true;
             }
-        }
-
-        return false;
-    }
-
-    public static boolean containsLowercaseLetter(String password) {
-        for (int i = 0; i < password.length(); i++) {
             if (Character.isLowerCase(password.charAt(i))) {
-                return true;
+                containsLowercaseLetter = true;
             }
-        }
-
-        return false;
-    }
-
-    public static boolean containsDigit(String password) {
-        for (int i = 0; i < password.length(); i++) {
             if (Character.isDigit(password.charAt(i))) {
-                return true;
+                containsDigit = true;
+            }
+            if (!Character.isDigit(password.charAt(i)) && !Character.isLetter(password.charAt(i))) {
+                containsSpecialSymbol = true;
             }
         }
 
-        return false;
-    }
-
-    public static boolean containsSpecialSymbol(String password) {
-        String specialChars = "~`!@#$%^&*()-_=+\\|[{]};:'\",<.>/?";
-        for (int i = 0; i < password.length(); i++) {
-            if (specialChars.contains(String.valueOf(password.charAt(i)))) {
-                return true;
-            }
+        if (!containsUppercaseLetter) {
+            return PasswordError.PASSWORD_NOT_CONTAINS_UPPERCASE_LETTER;
+        }
+        if (!containsLowercaseLetter) {
+            return PasswordError.PASSWORD_NOT_CONTAINS_LOWERCASE_LETTER;
+        }
+        if (!containsDigit) {
+            return PasswordError.PASSWORD_NOT_CONTAINS_DIGIT;
+        }
+        if (!containsSpecialSymbol) {
+            return PasswordError.PASSWORD_NOT_CONTAINS_SPECIAL_SYMBOL;
         }
 
-        return false;
+        return PasswordError.PASSWORD_IS_CORRECT;
     }
 
     public static boolean containsForbiddenWords(String password) {

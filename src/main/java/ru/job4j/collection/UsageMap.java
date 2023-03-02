5c7abdb130d1856afe4864c1413e398e6e78c6b1
viktorbeznosov <viktorbeznosov@mail.ru>
2023-03-02 21:39:53 +0300

    0. Map, HashMap [#212652 #396234]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,7 +7,10 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.put("parsentev@yandex.ru", "Petr Arsentev");
         map.put("viktorbeznosov@mail.ru", "Viktor Beznosov");
+        map.put("viktorbeznosov@mail.ru", "Viktor Georgievich Beznosov");
+        map.put("someone@mail.ru", "Some Body");
         for (String key: map.keySet()) {
             System.out.println(key + " - " + map.get(key));
         }

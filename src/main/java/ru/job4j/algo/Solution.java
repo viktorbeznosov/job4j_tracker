62bfdab67aebc9bc005c2810db210db762be18cb
viktorbeznosov <viktorbeznosov@mail.ru>
2025-11-30 19:19:53 +0300

    Algoritms

--- src/main/java/ru/job4j/algo/Solution.java
+++ src/main/java/ru/job4j/algo/Solution.java
@@ -1,10 +1,15 @@
 package ru.job4j.algo;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Solution {
+
     public static int[] sortedSquares(int[] nums) {
         int length = nums.length;
         int[] result = new int[length];
@@ -242,8 +247,385 @@ public class Solution {
         return builder.toString();
     }
 
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ArrayList<Integer> list = new ArrayList<>();
+        ListNode list1Current = list1;
+        ListNode list2Current = list2;
+        while (list1Current != null || list2Current != null) {
+            if (list2Current == null || list1Current != null && list1Current.val <= list2Current.val) {
+                list.add(list1Current.val);
+                list1Current = list1Current.next;
+            }
+            if (list1Current == null || list2Current != null && list2Current.val < list1Current.val) {
+                list.add(list2Current.val);
+                list2Current = list2Current.next;
+            }
+        }
+
+        ListNode next = null;
+        ListNode current = null;
+        for (int i = list.size() - 1; i >= 0; i--) {
+            current = new ListNode(list.get(i), next);
+            next = current;
+        }
+
+        return current;
+    }
+
+    public static int[] removeDuplicates(int[] nums) {
+        int[] result = new int[nums.length];
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+        AtomicInteger count = new AtomicInteger(0);
+        set.forEach(s -> {
+            result[count.getAndIncrement()] = s;
+        });
+
+        return result;
+    }
+
+    public static int[] removeElement(int[] nums, int val) {
+        List<Integer> tmp = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                tmp.add(nums[i]);
+            }
+        }
+        int[] result = new int[nums.length];
+        int count = 0;
+        for (int item: tmp) {
+            result[count] = item;
+            count++;
+        }
+
+        return result;
+    }
+
+    public static int strStr(String haystack, String needle) {
+        int index = -1;
+        if (needle.length() > haystack.length()) {
+            return index;
+        }
+        int i = 0;
+        int j = 0;
+
+        while (i < haystack.length()) {
+            if (haystack.charAt(i) == needle.charAt(0)) {
+                index = i;
+                while (j < needle.length() && (i + j) < haystack.length() && needle.charAt(j) == haystack.charAt(i + j)) {
+                    j++;
+                }
+                if (j == needle.length()) {
+                    return index;
+                } else {
+                    j = 0;
+                    index = -1;
+                }
+            }
+
+            i++;
+        }
+
+        return index;
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        int count = 0;
+        while (count < nums.length && target > nums[count]) {
+            count++;
+        }
+
+        return count;
+    }
+
+    public static int lengthOfLastWord(String s) {
+        s = s.replaceAll("\s+", " ").trim();
+        List<String> list = Arrays.stream(s.split(" ")).toList();
+
+        return list.get(list.size() - 1).length();
+    }
+
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
+        }
+
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        for (int i = 0; i < digits.length; i++) {
+            result[i + 1] = digits[i];
+        }
+
+        return result;
+    }
+
+    public static String addBinary(String a, String b) {
+        int maxLength = Math.max(a.length(), b.length());
+        if (a.length() != b.length()) {
+            int difference = Math.abs(a.length() - b.length());
+            String prefix = "0".repeat(difference);
+            if (a.length() > b.length()) {
+                b = prefix + b;
+            } else {
+                a = prefix + a;
+            }
+        }
+        StringBuilder builder = new StringBuilder();
+        char result = '0';
+        int carry = 0;
+        for (int i = maxLength - 1; i >= 0; i--) {
+            if (a.charAt(i) == '1' && b.charAt(i) == '1') {
+                result = carry == 0 ? '0' : '1';
+                carry = 1;
+            } else if (a.charAt(i) == '1' && b.charAt(i) == '0' || a.charAt(i) == '0' && b.charAt(i) == '1') {
+                result = carry == 0 ? '1' : '0';
+            } else {
+                result = carry == 0 ? '0' : '1';
+                carry = 0;
+            }
+            builder.append(result);
+        }
+        if (carry == 1) {
+            builder.append('1');
+        }
+
+        return builder.reverse().toString();
+    }
+
+    public static int mySqrt(int x) {
+        if (x == 0) {
+            return x;
+        }
+        var max = 2.147483648E9;
+        var current = max / 2;
+
+        while (!(current * current <= x) && !((current + 1) * (current + 1) < x)) {
+            if (current * current > x) {
+                max = current;
+                current = (int) Math.ceil((float) current / 2);
+            } else {
+                current = (int) Math.ceil(((float) current + max) / 2);
+            }
+        }
+
+        while ((current + 1) * (current + 1) <= x) {
+            if (x - (current + 1) * (current + 1) < (x - current * current)) {
+                current += 1;
+            }
+        }
+
+        return (int) current;
+    }
+
+    public static int climbStairs(int n) {
+        int[] fibbonaci = new int[]{0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986, 102334155, 165580141, 267914296, 433494437, 701408733, 1134903170, 1836311903
+        };
+
+        return fibbonaci[n];
+    }
+
+    public static int[] merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] result = new int[m + n];
+        int i = 0;
+        int j = 0;
+        int count = 0;
+
+        if (m == 0) {
+            for (int k = 0; k < nums2.length; k++) {
+                nums1[k] = nums2[k];
+            }
+        } else if (m > 0 && n > 0) {
+            if (nums1.length > 0 && nums2.length > 0) {
+                while (i < m || j < n) {
+                    if (j == nums2.length || i < m && nums1[i] <= nums2[j]) {
+                        result[count] = nums1[i];
+                        i++;
+                    } else if (i == nums1.length || j < n) {
+                        result[count] = nums2[j];
+                        j++;
+                    }
+                    count++;
+                }
+            }
+
+            for (int k = 0; k <  result.length; k++) {
+                nums1[k] = result[k];
+            }
+        }
+
+        return nums1;
+    }
+
+    public static class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+
+      TreeNode() {
+
+      }
+
+      TreeNode(int val) {
+          this.val = val;
+      }
+
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        return inorderTraversal(root, result);
+    }
+
+    public static List<Integer> inorderTraversal(TreeNode localRoot, List<Integer> list) {
+        if (localRoot != null) {
+            inorderTraversal(localRoot.left, list);
+            list.add(localRoot.val);
+            inorderTraversal(localRoot.right, list);
+        }
+        return list;
+    }
+
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        boolean result = true;
+        return isSameTree(p, q, result);
+    }
+
+    public static boolean isSameTree(TreeNode p, TreeNode q, boolean result) {
+        if (p == null && q != null || p != null && q == null) {
+            return false;
+        }
+        if (p != null && q != null) {
+            result = isSameTree(p.left, q.left, result);
+            if (p.val != q.val) {
+                return false;
+            }
+            result = isSameTree(p.right, q.right, result);
+        }
+        return result;
+    }
+
+    public static boolean isSymmetric(TreeNode root) {
+        return isSameTree(root, getMirrorTree(root));
+    }
+
+    public static TreeNode getMirrorTree(TreeNode root) {
+        TreeNode current = new TreeNode(root.val);
+        TreeNode temp = root.left;
+        current.left = root.right;
+        current.right = temp;
+        if (current.left != null) {
+            current.left = getMirrorTree(current.left);
+        }
+        if (current.right != null) {
+            current.right = getMirrorTree(current.right);
+        }
+        return current;
+    }
+
+    public static int maxDepth(TreeNode root) {
+        int depth = 1;
+        if (root == null) {
+            return 0;
+        }
+        return maxDepth(root, depth);
+    }
+
+    public static int maxDepth(TreeNode root, int depth) {
+        if (root == null) {
+            return depth - 1;
+        }
+
+        return Math.max(maxDepth(root.left, depth + 1), maxDepth(root.right, depth + 1));
+    }
+
+    public static int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+
+        if (root.left == null) {
+            return 1 + minDepth(root.right);
+        }
+
+        if (root.right == null) {
+            return 1 + minDepth(root.left);
+        }
+
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+
+    public static boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root != null) {
+            if (root.left == null && root.right == null) {
+                return root.val - targetSum == 0;
+            }
+            boolean result = hasPathSum(root.left, targetSum - root.val);
+            return (result)
+                ? hasPathSum(root.left, targetSum - root.val)
+                : hasPathSum(root.right, targetSum - root.val);
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) {
-        String[] strs = new String[]{"dog", "racecar", "car"};
-        System.out.println(longestCommonPrefix(strs));
+        TreeNode treeNode = new TreeNode(
+                5,
+                new TreeNode(
+                        4,
+                        new TreeNode(
+                                11,
+                                new TreeNode(7),
+                                new TreeNode(2)
+                        ),
+                        null),
+                new TreeNode(
+                        8,
+                        new TreeNode(13),
+                        new TreeNode(
+                                4,
+                                null,
+                                new TreeNode(1)
+                        )
+                )
+        );
+
+//        TreeNode treeNode = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+
+        System.out.println(hasPathSum(treeNode, 26));
     }
 }

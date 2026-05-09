7ae3ded283083c7701ce106348908a29da3fe822
viktorbeznosov <viktorbeznosov@mail.ru>
2026-05-09 18:47:12 +0300

    1. Конфигурация Hibernate [#6872]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,12 +1,20 @@
 package ru.job4j.tracker;
 
+import lombok.Data;
+import javax.persistence.*;
+
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@Entity
+@Table(name = "items")
+@Data
 public class Item {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
